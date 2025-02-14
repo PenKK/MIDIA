4823bf8543250caa105e3057fa65c318137c1ccb
Arman Bains <abains32@students.cs.ubc.ca>
2025-02-14 10:42:18 -0800

    fail method gives context in testMidiInvalidDataException()

--- src/test/model/TestMidiTrack.java
+++ src/test/model/TestMidiTrack.java
@@ -321,7 +321,7 @@ public class TestMidiTrack {
         Track t = sequence.createTrack();
         try {
             midiTrack.applyToTrack(t);
-            fail();
+            fail("Invalid midi data was not detected");
         } catch(RuntimeException e) {
             // success
         }
