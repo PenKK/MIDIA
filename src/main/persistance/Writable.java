03fe361b7c9e2c8513f4b035da195f63e19cded5
Arman Bains <abains32@students.cs.ubc.ca>
2025-02-17 23:38:03 -0800

    forgot citation for writable interface

--- src/main/persistance/Writable.java
+++ src/main/persistance/Writable.java
@@ -2,6 +2,8 @@ package persistance;
 
 import org.json.JSONObject;
 
+// Code adapted from src/main/persistance/Writable
+//     at https://github.students.cs.ubc.ca/CPSC210/JsonSerializationDemo
 public interface Writable {
     // EFFECTS: returns the object as a JSON object
     JSONObject toJson();
