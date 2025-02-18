660c7d07f5d46ceaf7766a9b0e7f16ef63d745bf
Arman Bains <abains32@students.cs.ubc.ca>
2025-02-17 22:38:47 -0800

    specification for JsonWriter

---
+++ src/main/persistance/JsonWriter.java
@@ -0,0 +1,40 @@
+package persistance;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+
+import model.Timeline;
+
+// Represents a writer that writes JSON representation of Timeline to file
+// Code adapted from src/main/persistance/JsonWriter
+//     at https://github.students.cs.ubc.ca/CPSC210/JsonSerializationDemo
+public class JsonWriter {
+
+    private static final int TAB_SPACING = 4;
+    private String path;
+    private PrintWriter writer;
+
+    // EFFECTS: constructs writer that writes at the specified path
+    public JsonWriter(String path) {
+        // stub
+    }
+
+    // MODIFIES: this
+    // EFFECTS: opens writer; throws FileNotFoundException if the path is not found/accessible
+    //          for writing
+    public void open() throws FileNotFoundException {
+        // stub
+    }
+
+    // MODIFIES: this
+    // EFFECTS: writes JSON representation of timeline to path
+    public void write(Timeline timeline) {
+        // stub
+    }
+
+    // EFFECTS: closes the writer
+    public void close() {
+        // stub
+    }
+}
