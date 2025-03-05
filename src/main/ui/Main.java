e26579c286961ef4d9cd45e2d4c0a9b3b1630716
Arman Bains <abains32@students.cs.ubc.ca>
2025-03-04 16:18:05 -0800

    Entry point of application changed to swing UI

--- src/main/ui/Main.java
+++ src/main/ui/Main.java
@@ -1,8 +1,8 @@
 package ui;
 
-// Launches the Daw, entry point of application
+// Launches the Daw UI, entry point of application
 public class Main {
     public static void main(String[] args) throws Exception {
-        new DAW();
+        new DawFrame(); 
     }
 }
