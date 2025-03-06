babac33abeadcbf6c3a293c3768530d8b8f33be2
Arman Bains <abains32@students.cs.ubc.ca>
2025-03-06 11:58:55 -0800

    observer pattern implementation

--- src/main/ui/tabs/timeline/TimelinePanel.java
+++ src/main/ui/tabs/timeline/TimelinePanel.java
@@ -1,8 +1,51 @@
 package ui.tabs.timeline;
 
+import java.beans.PropertyChangeEvent;
+import java.beans.PropertyChangeListener;
+import java.util.ArrayList;
+
+
 import javax.swing.JPanel;
 
-// JPanel that holds the interactable view of the timeline, rendered using graphics
-public class TimelinePanel extends JPanel {
+import model.MidiTrack;
+import model.Timeline;
 
+// JPanel that holds the interactable view of the timeline, rendered using graphics
+public class TimelinePanel extends JPanel implements PropertyChangeListener {
+
+    ArrayList<MidiTrackPanel> midiTrackPanels;
+    Timeline timeline;
+
+    // EFFECTS: initializes the timeline 
+    public TimelinePanel() {
+        this.setName("Timeline");
+        timeline = Timeline.getInstance();
+        midiTrackPanels = new ArrayList<>();
+        Timeline.addObserver(this);
+
+        if (timeline == null) {
+            System.err.println("Timeline instance returned null, MIDI device may be unavliable");
+            return;
+        }
+
+        populateRows();
+    }
+
+    // EFFECTS: clears all MidiTrackPanels and then populates according to current Timeline
+    private void populateRows() {
+        midiTrackPanels.clear();
+        for (MidiTrack track : timeline.getTracks()) {
+            midiTrackPanels.add(new MidiTrackPanel(track));
+        }
+    }
+
+    @Override
+    public void propertyChange(PropertyChangeEvent evt) {
+        String propertyName = evt.getPropertyName();
+
+        if (propertyName.equals("timeline")) {
+            timeline = Timeline.getInstance();
+            populateRows();
+        }
+    }
 }
