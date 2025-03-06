f7cd87f93b39de4290802197e9ec1d7edd2b84ce
Arman Bains <abains32@students.cs.ubc.ca>
2025-03-06 12:46:48 -0800

    Tabbed pane constructor

--- src/main/ui/tabs/TabbedPane.java
+++ src/main/ui/tabs/TabbedPane.java
@@ -2,9 +2,20 @@ package ui.tabs;
 
 import javax.swing.JTabbedPane;
 
+import ui.tabs.piano.roll.PianoRollPanel;
 import ui.tabs.timeline.TimelinePanel;
 
 // TabbedPane holds the tabs of the TimelinePanel and PianoRollPanel
 public class TabbedPane extends JTabbedPane {
 
+    TimelinePanel timelinePanel;
+    PianoRollPanel pianoRollPanel;
+
+    // EFFECTS: Initialized the pane with the timeline and pianoroll tabs
+    public TabbedPane() {
+        timelinePanel = new TimelinePanel();
+        pianoRollPanel = new PianoRollPanel();
+        this.add(timelinePanel);
+        this.add(pianoRollPanel);
+    }
 }
