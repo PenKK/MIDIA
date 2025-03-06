85a6417e7325fdc18bd33419e4dffc9ec69dae50
Arman Bains <abains32@students.cs.ubc.ca>
2025-03-06 13:34:01 -0800

    Frame now updates title according to loaded project

--- src/main/ui/DawFrame.java
+++ src/main/ui/DawFrame.java
@@ -1,16 +1,19 @@
 package ui;
 
 import java.awt.Rectangle;
+import java.beans.PropertyChangeEvent;
+import java.beans.PropertyChangeListener;
 import java.io.IOException;
 
 import javax.sound.midi.MidiUnavailableException;
 import javax.swing.JFrame;
 
+import model.Timeline;
 import ui.menubar.MenuBar;
 import ui.tabs.TabbedPane;
 
 // The frame of the graphical UI. Contains the entirety of the UI.
-public class DawFrame extends JFrame {
+public class DawFrame extends JFrame implements PropertyChangeListener {
 
     private MenuBar menuBar;
     private TabbedPane tabbedPane;
@@ -19,6 +22,7 @@ public class DawFrame extends JFrame {
     DawFrame() throws MidiUnavailableException, IOException {
         tabbedPane = new TabbedPane();
         menuBar = new MenuBar();
+        Timeline.addObserver(this);
 
         this.setDefaultCloseOperation(EXIT_ON_CLOSE);
         this.setTitle("Digital Audio Workstation");
@@ -29,5 +33,19 @@ public class DawFrame extends JFrame {
 
         this.setLocationRelativeTo(null);
         this.setVisible(true);
+    }
+
+    @Override
+    public void propertyChange(PropertyChangeEvent evt) {
+        String propertyName = evt.getPropertyName();
+        
+        if (propertyName.equals("timeline")) {
+            updateTitle();
+        }
+    }
+
+    private void updateTitle() {
+        String newTitle = Timeline.getInstance().getProjectName().concat(" - Digital Audio Workstation");
+        this.setTitle(newTitle);
     }
 }
