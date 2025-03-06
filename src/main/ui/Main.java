418e5cf7c092a336241a974d93eb21dc9ff8298b
Arman Bains <abains32@students.cs.ubc.ca>
2025-03-06 13:10:25 -0800

    FlatLaf library

--- src/main/ui/Main.java
+++ src/main/ui/Main.java
@@ -1,8 +1,11 @@
 package ui;
 
+import com.formdev.flatlaf.FlatDarculaLaf;
+
 // Launches the Daw UI, entry point of application
 public class Main {
     public static void main(String[] args) throws Exception {
+        // FlatDarculaLaf.setup();
         new DawFrame(); 
     }
 }
