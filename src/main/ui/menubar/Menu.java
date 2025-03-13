c6450cba65f4e54efa7d7e3c9e88e896880afa02
Arman Bains <abains32@students.cs.ubc.ca>
2025-03-12 18:47:15 -0700

    unused imports

--- src/main/ui/menubar/Menu.java
+++ src/main/ui/menubar/Menu.java
@@ -1,13 +1,6 @@
 package ui.menubar;
 
-import java.util.ArrayList;
-import java.util.List;
-
-import javax.swing.JComponent;
 import javax.swing.JMenu;
-import javax.swing.JPopupMenu;
-import javax.swing.event.PopupMenuEvent;
-import javax.swing.event.PopupMenuListener;
 
 // Represents a Menu in the MenuBar
 public class Menu extends JMenu {
