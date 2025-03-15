2d11bacc0414f62e96ff83fed4c6e2c58ad2423b
Arman Bains <abains32@students.cs.ubc.ca>
2025-03-15 02:45:53 -0700

    testing coverage for enums

---
+++ src/test/model/TestInstrument.java
@@ -0,0 +1,34 @@
+package model;
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+public class TestInstrument {
+
+    @Test
+    void testGetters() {
+        int num = 0;
+        for (Instrument i : InstrumentalInstrument.values()) {
+            assertEquals(i.getProgramNumber(), num);
+            num++;
+        }
+        
+        assertEquals(num, 128);
+
+        num = 35;
+        for (Instrument i : PercussionInstrument.values()) {
+            assertEquals(i.getProgramNumber(), num);
+            num++;
+        }
+        
+        assertEquals(num, 82);
+        assertEquals(InstrumentalInstrument.ALTO_SAX.getName(), "Alto Sax");
+        assertEquals(PercussionInstrument.ACOUSTIC_BASS_DRUM.getName(), "Acoustic Bass Drum");
+
+        assertEquals(InstrumentalInstrument.BARITONE_SAX.getName(), 
+                     InstrumentalInstrument.BARITONE_SAX.toString());
+
+        assertEquals(PercussionInstrument.CHINESE_CYMBAL.getName(), 
+        PercussionInstrument.CHINESE_CYMBAL.toString());
+    }
+}
