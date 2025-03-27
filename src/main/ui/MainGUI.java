0a54a7811fbceb4d2fe992fbf590ec73ba594d55
Arman Bains <abains32@students.cs.ubc.ca>
2025-03-26 19:03:16 -0700

    FlatLaf officially approved

--- src/main/ui/MainGUI.java
+++ src/main/ui/MainGUI.java
@@ -1,26 +1,20 @@
 package ui;
 
-import javax.swing.SwingUtilities;
-
 import com.formdev.flatlaf.FlatDarculaLaf;
-
-import model.Event;
-import model.EventLog;
 
 // Launches the Daw UI, entry point of application
 public class MainGUI {
     public static void main(String[] args) throws Exception {
-        if (args.length != 0 && args[0].toUpperCase().trim().equals("TRUE")) {
-            FlatDarculaLaf.setup(); 
+        // FlatLaf library for look and feel
+        // https://www.formdev.com/flatlaf/
+        FlatDarculaLaf.setup();
+
+        try {
+            new DawFrame();
+        } catch (Exception e) {
+            System.out.println("Unable to start program, MIDI device likely unavaliable");
+            e.printStackTrace();
         }
 
-        SwingUtilities.invokeLater(() -> {
-            try {
-                new DawFrame();
-            } catch (Exception e) {
-                System.out.println("Unable to start program, MIDI device likely unavaliable");
-                e.printStackTrace();
-            }
-        });
     }
 }
