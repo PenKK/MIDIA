eca0af1260a6b72e193fa7a42d9d10549eb576ef
PenK <spikeb3b@gmail.com>
2025-04-30 17:37:23 -0700

    Refactored a Player class out of Timeline, Timeline now has less responsibility

---
+++ src/main/model/Player.java
@@ -0,0 +1,254 @@
+package model;
+
+import java.util.ArrayList;
+
+import javax.sound.midi.InvalidMidiDataException;
+import javax.sound.midi.MidiSystem;
+import javax.sound.midi.MidiUnavailableException;
+import javax.sound.midi.Sequence;
+import javax.sound.midi.Sequencer;
+import javax.sound.midi.Track;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import persistance.Writable;
+
+public class Player implements Writable {
+
+    public static final int PULSES_PER_QUARTER_NOTE = 960;
+    private static final float DEFAULT_BPM = 120;
+
+    private Sequencer sequencer;
+    private Sequence sequence;
+    private Timeline timeline;
+
+    private float bpm;
+    private int positionTick;
+    private ArrayList<Integer> availableChannels;
+
+    public Player(Timeline timeline) throws MidiUnavailableException, InvalidMidiDataException {
+        bpm = DEFAULT_BPM;
+        positionTick = 0;
+        this.timeline = timeline;
+
+        sequencer = MidiSystem.getSequencer();
+        sequence = new Sequence(Sequence.PPQ, PULSES_PER_QUARTER_NOTE);
+        sequencer.open();
+
+
+        availableChannels = new ArrayList<>() {
+            {
+                for (int i = 0; i <= 15; i++) {
+                    if (i != 9) {
+                        add(i); // add numbers 0-15 excluding 9, 9 is for percussion
+                    }
+                }
+            }
+        };
+    }
+
+    public ArrayList<Integer> getAvailableChannels() {
+        return availableChannels;
+    }
+
+    // MODIFIES: this
+    // EFFECTS: updates the sequence with the current midiTracks, converting each one 
+    //          to a Java Track. Throws InvalidMidiDataException if invalid midi data
+    //          is found when setting the sequence to the sequencer
+    public void updateSequence() throws InvalidMidiDataException {
+        resetTracks();
+        for (MidiTrack currentMidiTrack : timeline.getTracks()) {
+            if (currentMidiTrack.isMuted() || currentMidiTrack.getVolume() == 0) {
+                continue;
+            }
+
+            Track track = sequence.createTrack();
+            currentMidiTrack.applyToTrack(track);
+        }
+
+        sequencer.setSequence(sequence);
+
+        Event e = new Event(String.format("Playback sequence was updated in Timeline %s", timeline.getProjectName()));
+        EventLog.getInstance().logEvent(e);
+    }
+
+    // MODIFIES: this
+    // EFFECTS: deletes all Tracks from the sequence, essentially resetting playback
+    public void resetTracks() {
+        for (Track track : sequence.getTracks()) {
+            sequence.deleteTrack(track);
+        }
+
+        Event e = new Event(String.format("Playback sequence cleared in Timeline %s", timeline.getProjectName()));
+        EventLog.getInstance().logEvent(e);
+    }
+
+    // MODIFIES: this
+    // EFFECTS: Begins playback at the current tick position and with tempo according to 
+    //          beatsPerMinute. Can throw InvalidMidiDataException if invalid
+    //          midi data is found during the sequence update (handled by UI)
+    public void play() throws InvalidMidiDataException {
+        updateSequence();
+        sequencer.setTickPosition(positionTick);
+        sequencer.setTempoInBPM(bpm);
+        sequencer.start();
+
+        Event e = new Event(String.format("Playback started, Sequence length: %d ticks",
+                sequencer.getTickLength()));
+        EventLog.getInstance().logEvent(e);
+    }
+
+    // MODIFIES: this
+    // EFFECTS: Pauses playback
+    public void pause() {
+        sequencer.stop();
+        updatePositionTick();
+
+        Event e = new Event(String.format("Playback paused in Timeline %s at tick: %d",
+                timeline.getProjectName(), sequencer.getTickPosition()));
+        EventLog.getInstance().logEvent(e);
+    }
+
+    // MODFIES: this
+    // EFFECTS: updates the position tick according to the current playback tick
+    public void updatePositionTick() {
+        setPositionTick((int) sequencer.getTickPosition());
+    }
+
+    // REQUIRES: newPositionTick >= 0
+    // MODIFIES: this
+    // EFFECTS: Changes timeline position to start playback given ticks
+    public void setPositionTick(int newPositionTick) {
+        int oldPositionTick = positionTick;
+        this.positionTick = newPositionTick;
+
+        timeline.getPropertyChangeSupport().firePropertyChange("positionTick", oldPositionTick, newPositionTick);
+    }
+
+    // MODIFIES: this
+    // EFFECTS: replaces current available channels with new ones, use wisely
+    //          channels may be out of sync with tracks 
+    public void setAvailableChannels(ArrayList<Integer> newChannels) {
+        availableChannels = newChannels;
+    }
+
+    // REQUIRES: newPositionMs >= 0
+    // MODIFIES: this
+    // EFFECTS: Changes timeline position to start playback at the given milliseconds
+    public void setPositionMs(double newPositionMs) {
+        setPositionTick(msToTicks(newPositionMs));
+    }
+
+    // REQUIRES: newPositionBeat >= 1
+    // MODIFIES: this
+    // EFFECTS: Changes timeline position to start playback given the beat to start at
+    public void setPositionBeat(double newPositionBeat) {
+        setPositionTick(beatsToTicks(newPositionBeat - 1));
+    }
+
+    // REQUIRES: bpm >= 1
+    // EFFECTS: changes the BPM
+    public void setBPM(float bpm) {
+        float oldBpm = bpm;
+        this.bpm = bpm;
+
+        timeline.getPropertyChangeSupport().firePropertyChange("bpm", oldBpm, bpm);
+    }
+
+    // REQUIRES: ticks >= 0
+    // EFFECTS: converts ticks to milliseconds given the BPM
+    public double ticksToMs(int ticks) {
+        double durationInQuarterNotes = (double) ticks / (double) sequence.getResolution();
+        double durationInMinutes = durationInQuarterNotes / bpm;
+        return durationInMinutes * 60000;
+    }
+
+    // REQUIRES: ms >= 0
+    // EFFECTS: converts milliseconds to ticks (reverse of above)
+    public int msToTicks(double ms) {
+        double durationInMinutes = ms / (double) 60000;
+        double durationInQuarterNotes = bpm * durationInMinutes;
+        double ticks = durationInQuarterNotes * sequence.getResolution();
+        return (int) Math.round(ticks);
+    }
+
+    // REQUIRES: beats >= 0
+    // EFFECTS: converts beats to ms
+    public int beatsToMs(double beats) {
+        return (int) Math.round(beats / bpm * 60 * 1000);
+    }
+
+    // REQUIRES: beats >= 0
+    // EFFECTS: calculates beats to ticks conversion
+    public int beatsToTicks(double beats) {
+        double ticks = beats * (double) sequence.getResolution();
+        return (int) Math.round(ticks);
+    }
+
+    // REQUIRES: ticks >= 0
+    // EFFECTS: calculates ticks to beats conversion (reverse of above)
+    public double ticksToBeats(int ticks) {
+        double beats = (double) ticks / (double) sequence.getResolution();
+        return beats;
+    }
+
+    // REQUIRES: ticks >= 0
+    // EFFECTS: converts the ticks to which beat the ticks are on
+    public double ticksToOnBeat(int ticks) {
+        return ticksToBeats(ticks) + 1;
+    }
+
+    // EFFECTS: returns the calculation of the sequence length in beats
+    public double getLengthBeats() {
+        return ticksToBeats(timeline.getLengthTicks());
+    }
+
+    // EFFECTS: returns the calculation of the sequence length in milliseconds
+    public double getLengthMs() {
+        return ticksToMs(timeline.getLengthTicks());
+    }
+
+    public int getPositionTick() {
+        return positionTick;
+    }
+
+    // EFFECTS: returns the timeline position in ms by converting ticks to ms
+    public double getPositionMs() {
+        return ticksToMs(positionTick);
+    }
+
+    // EFFECTS: returns the timeline position in beats by converting ticks to beats
+    public double getPositionBeats() {
+        return ticksToBeats(positionTick);
+    }
+
+    // EFFECTS: returns the beat on which the timeline position starts playback
+    public double getPositionOnBeat() {
+        return getPositionBeats() + 1;
+    }
+
+    public float getBPM() {
+        return bpm;
+    }
+
+    public Sequence getSequence() {
+        return sequence;
+    }
+
+    public Sequencer getSequencer() {
+        return sequencer;
+    }
+
+    @Override
+    public JSONObject toJson() {
+        JSONObject playerJson = new JSONObject();
+
+        playerJson.put("beatsPerMinute", bpm);
+        playerJson.put("positionTick", positionTick);
+        playerJson.put("availableChannels", new JSONArray(availableChannels));
+
+        return playerJson;
+    }
+
+}
--- src/main/model/Timeline.java
+++ src/main/model/Timeline.java
@@ -5,11 +5,7 @@ import java.beans.PropertyChangeSupport;
 import java.util.ArrayList;
 
 import javax.sound.midi.InvalidMidiDataException;
-import javax.sound.midi.MidiSystem;
 import javax.sound.midi.MidiUnavailableException;
-import javax.sound.midi.Sequence;
-import javax.sound.midi.Sequencer;
-import javax.sound.midi.Track;
 
 import org.json.JSONArray;
 import org.json.JSONObject;
@@ -23,26 +19,18 @@ import persistance.Writable;
 // Higher level MidiTrack(s) will be converted to the lower level Java Track for playback
 public class Timeline implements Writable {
 
-    public static final int PULSES_PER_QUARTER_NOTE = 960;
     public static final double MAX_HORIZONTAL_SCALE = 1;
     public static final double MIN_HORIZONTAL_SCALE = 0.02;
 
-    private static final float DEFAULT_BPM = 120;
     private static final int DEFAULT_BEAT_DIVISON = 4;
     private static final int DEFAULT_BEATS_PER_MEASURE = 4;
     private static final double DEFAULT_HORIZONTAL_SCALE = 0.1;
 
-    private static Timeline instance; // singleton 
-    private static PropertyChangeSupport pcs = new PropertyChangeSupport(Timeline.class);
-
-    private Sequencer sequencer;
-    private Sequence sequence;
+    private PropertyChangeSupport pcs = new PropertyChangeSupport(Timeline.class);
 
     private String projectName;
     private ArrayList<MidiTrack> midiTracks;
-    private float bpm;
-    private int positionTick;
-    private ArrayList<Integer> avaliableChannels;
+    private Player player;
 
     private int beatDivision;
     private int beatsPerMeasure;
@@ -51,32 +39,16 @@ public class Timeline implements Writable {
     // EFFECTS: Creates a timeline with a single sequence with no tracks and the positon 
     //          tick at 0, and a BPM of 120.
     //          Method throws MidiUnavailableException if the device has no MIDI sequencer
-    //          avaliable, which is fatal and unrecoverable.
+    //          available, which is fatal and unrecoverable.
     //          Method throws InvalidMidiDataException if the Sequence has an invalid 
     //          divison type.
-    public Timeline(String projectName) throws MidiUnavailableException, InvalidMidiDataException {
+    public Timeline(String projectName) throws MidiUnavailableException, InvalidMidiDataException  {
         this.projectName = projectName;
-        sequencer = MidiSystem.getSequencer();
-        sequence = new Sequence(Sequence.PPQ, PULSES_PER_QUARTER_NOTE);
-        sequencer.open();
-
-        bpm = DEFAULT_BPM;
-        midiTracks = new ArrayList<>();
-        positionTick = 0;
-
         beatDivision = DEFAULT_BEAT_DIVISON;
         beatsPerMeasure = DEFAULT_BEATS_PER_MEASURE;
         horizontalScale = DEFAULT_HORIZONTAL_SCALE;
-
-        avaliableChannels = new ArrayList<>() {
-            {
-                for (int i = 0; i <= 15; i++) {
-                    if (i != 9) {
-                        add(i); // add numbers 0-15 excluding 9, 9 is for percussion
-                    }
-                }
-            }
-        };
+        player = new Player(this);
+        midiTracks = new ArrayList<>();
         
         Event e = new Event(String.format("A new timeline instance was created with project name: %s", 
                                           projectName));
@@ -84,54 +56,32 @@ public class Timeline implements Writable {
     }
 
     // MODIFIES: this
-    // EFFECTS: Initializes the singeton instance and returns it. 
-    //          Returns null if no MidiDevice is unavaliable
-    public static Timeline getInstance() {
-        if (instance == null) {
-            try {
-                instance = new Timeline("New Project");
-            } catch (MidiUnavailableException | InvalidMidiDataException e) {
-                return null;
-            }
-        }
-        return instance;
-    }
-
-    // MODIFIES: this
-    // EFFECTS: Replaces instance and fires property change for all who need to reflect changes
-    public static void setInstance(Timeline newInstance) {
-        Timeline oldInstance = instance;
-        instance = newInstance;
-        pcs.firePropertyChange("timeline", oldInstance, newInstance);
-        oldInstance.getSequencer().close();
-
-        Event e = new Event(String.format("Timeline singleton instance was replaced, old name: %s, new name: %s ", 
-                                          oldInstance.getProjectName(), newInstance.getProjectName()));
-        EventLog.getInstance().logEvent(e);
-    }
-
-    // MODIFIES: this
     // EFFECTS: adds the specified observer as a listener of property changes
-    public static void addObserver(PropertyChangeListener observer) {
+    public void addObserver(PropertyChangeListener observer) {
         pcs.addPropertyChangeListener(observer);
     }
 
     // MODIFIES: this
     // EFFECTS: removes the specified observer as a listener of property changes
-    public static void removeObserver(PropertyChangeListener observer) {
+    public void removeObserver(PropertyChangeListener observer) {
         pcs.removePropertyChangeListener(observer);
     }
 
-    // REQUIRES: avaliableChannels.size() >= 0
+    // EFFECTS: forces a timeline update (for rendering purposes one day better fix hopefuly)
+    public void refresh() {
+        pcs.firePropertyChange("timeline", null, this);
+    }
+
+    // REQUIRES: player.getAvailableChannels().size() >= 0
     // MODIFIES: this
     // EFFECTS: Creates a midiTrack, add its to the list of tracks and returns it
     public MidiTrack createMidiTrack(String name, Instrument instrument, boolean percussive) {
-        if (!percussive && avaliableChannels.size() <= 0) {
+        if (!percussive && player.getAvailableChannels().size() <= 0) {
             return null;
         }
 
         MidiTrack newMidiTrack = new MidiTrack(name, instrument,
-                percussive ? 9 : avaliableChannels.remove(0));
+                percussive ? 9 : player.getAvailableChannels().remove(0));
 
         ArrayList<MidiTrack> oldTracks = new ArrayList<>(midiTracks);
         midiTracks.add(newMidiTrack);
@@ -140,7 +90,7 @@ public class Timeline implements Writable {
         Event e = new Event(String.format("Created new MidiTrack, instrument: %s, channel: %d, percussive: %b. "
                                         + "Remaining instrumental channels: %d",
                                           instrument, newMidiTrack.getChannel(), 
-                                          newMidiTrack.isPercussive(), avaliableChannels.size()));
+                                          newMidiTrack.isPercussive(), player.getAvailableChannels().size()));
         EventLog.getInstance().logEvent(e);
 
         return newMidiTrack;
@@ -149,132 +99,31 @@ public class Timeline implements Writable {
     // REQUIRES: index >= 0 and midiTracks.size() > 0
     // MODIFIES: this
     // EFFECTS: removes the MidiTrack at the specified index from midiTracks, adds the
-    //          channel to avaliable channels if the MidiTrack is not percussive, and then
+    //          channel to available channels if the MidiTrack is not percussive, and then
     //          returns the MidiTrack
     public MidiTrack removeMidiTrack(int index) {
         ArrayList<MidiTrack> oldTracks = new ArrayList<>(midiTracks);
         MidiTrack removed = midiTracks.remove(index);
 
         if (!removed.isPercussive()) {
-            avaliableChannels.add(removed.getChannel());
+            player.getAvailableChannels().add(removed.getChannel());
         }
 
         pcs.firePropertyChange("midiTracks", oldTracks, new ArrayList<>(midiTracks));
 
         Event e = new Event(String.format("Removed MidiTrack[%d]: %s. Remaining instrumental channels: %d",
-                                        index, removed, avaliableChannels.size()));
+                                        index, removed, player.getAvailableChannels().size()));
         EventLog.getInstance().logEvent(e);
 
         return removed;
     }
 
-    // EFFECTS: forces a timeline update (for rendering purposes one day better fix hopefuly)
-    public static void refresh() {
-        pcs.firePropertyChange("timeline", null, getInstance());
-    }
-
-    // MODIFIES: this
-    // EFFECTS: updates the sequence with the current midiTracks, converting each one 
-    //          to a Java Track. Throws InvalidMidiDataException if invalid midi data
-    //          is found when setting the sequence to the sequencer
-    public void updateSequence() throws InvalidMidiDataException {
-        resetSequenceTracks();
-        for (MidiTrack currentMidiTrack : midiTracks) {
-            if (currentMidiTrack.isMuted() || currentMidiTrack.getVolume() == 0) {
-                continue;
-            }
-
-            Track track = sequence.createTrack();
-            currentMidiTrack.applyToTrack(track);
-        }
-
-        sequencer.setSequence(sequence);
-
-        Event e = new Event(String.format("Playback sequence was updated in Timeline %s", projectName));
-        EventLog.getInstance().logEvent(e);
-    }
-
-    // MODIFIES: this
-    // EFFECTS: deletes all Tracks from the sequence, essentially resetting playback
-    public void resetSequenceTracks() {
-        for (Track track : sequence.getTracks()) {
-            sequence.deleteTrack(track);
-        }
-
-        Event e = new Event(String.format("Playback sequence cleared Timeline %s", projectName));
-        EventLog.getInstance().logEvent(e);
-    }
-
-    // MODIFIES: this
-    // EFFECTS: Begins playback at the current tick position and with tempo according to 
-    //          beatsPerMinute. Can throw InvalidMidiDataException if invalid
-    //          midi data is found during the sequence update (handled by UI)
     public void play() throws InvalidMidiDataException {
-        updateSequence();
-        sequencer.setTickPosition(positionTick);
-        sequencer.setTempoInBPM(bpm);
-        sequencer.start();
-
-        Event e = new Event(String.format("Playback started in Timeline %s. Sequence length: %d ticks",
-                                           projectName, sequencer.getTickLength()));
-        EventLog.getInstance().logEvent(e);
+        player.play();
     }
 
-    // MODIFIES: this
-    // EFFECTS: Pauses playback
     public void pause() {
-        sequencer.stop();
-        updatePositionTick();
-
-        Event e = new Event(String.format("Playback paused in Timeline %s at tick: %d",
-                                           projectName, sequencer.getTickPosition()));
-        EventLog.getInstance().logEvent(e);
-    }
-
-    // MODFIES: this
-    // EFFECTS: updates the position tick according to the current playback tick
-    public void updatePositionTick() {
-        setPositionTick((int) sequencer.getTickPosition());
-    }
-
-    // MODIFIES: this
-    // EFFECTS: replaces current avaliable channels with new ones, use wisely
-    //          channels may be out of sync with tracks 
-    public void setAvaliableChannels(ArrayList<Integer> newChannels) {
-        avaliableChannels = newChannels;
-    }
-
-    // REQUIRES: newPositionTick >= 0
-    // MODIFIES: this
-    // EFFECTS: Changes timeline position to start playback given ticks
-    public void setPositionTick(int newPositionTick) {
-        int oldPositionTick = positionTick;
-        this.positionTick = newPositionTick;
-
-        pcs.firePropertyChange("positionTick", oldPositionTick, newPositionTick);
-    }
-
-    // REQUIRES: newPositionMs >= 0
-    // MODIFIES: this
-    // EFFECTS: Changes timeline position to start playback at the given milliseconds
-    public void setPositionMs(double newPositionMs) {
-        setPositionTick(msToTicks(newPositionMs));
-    }
-
-    // REQUIRES: newPositionBeat >= 1
-    // MODIFIES: this
-    // EFFECTS: Changes timeline position to start playback given the beat to start at
-    public void setPositionBeat(double newPositionBeat) {
-        setPositionTick(beatsToTicks(newPositionBeat - 1));
-    }
-
-    // REQUIRES: bpm >= 1
-    // EFFECTS: changes the BPM
-    public void setBPM(float bpm) {
-        float oldBpm = bpm;
-        this.bpm = bpm;
-
-        pcs.firePropertyChange("bpm", oldBpm, bpm);
+        player.pause();
     }
 
     // MODIFIES: this
@@ -310,59 +159,8 @@ public class Timeline implements Writable {
         pcs.firePropertyChange("horizontalScale", oldHorizontalScale, newHorizontalScale);
     }
 
-    // EFFECTS: returns the calculation of the sequence length in milliseconds
-    public double getLengthMs() {
-        return ticksToMs(getLengthTicks());
-    }
-
-    // EFFECTS: returns the calculation of the sequence length in beats
-    public double getLengthBeats() {
-        return ticksToBeats(getLengthTicks());
-    }
-
-    // REQUIRES: ticks >= 0
-    // EFFECTS: converts ticks to milliseconds given the BPM
-    public double ticksToMs(int ticks) {
-        double durationInQuarterNotes = (double) ticks / (double) sequence.getResolution();
-        double durationInMinutes = durationInQuarterNotes / bpm;
-        return durationInMinutes * 60000;
-    }
-
-    // REQUIRES: ms >= 0
-    // EFFECTS: converts milliseconds to ticks (reverse of above)
-    public int msToTicks(double ms) {
-        double durationInMinutes = ms / (double) 60000;
-        double durationInQuarterNotes = bpm * durationInMinutes;
-        double ticks = durationInQuarterNotes * sequence.getResolution();
-        return (int) Math.round(ticks);
-    }
-
-    // REQUIRES: beats >= 0
-    // EFFECTS: converts beats to ms
-    public int beatsToMs(double beats) {
-        return (int) Math.round(beats / bpm * 60 * 1000);
-    }
-
-    // REQUIRES: beats >= 0
-    // EFFECTS: calculates beats to ticks conversion
-    public int beatsToTicks(double beats) {
-        double ticks = beats * (double) sequence.getResolution();
-        ;
-        return (int) Math.round(ticks);
-    }
-
-    // REQUIRES: ticks >= 0
-    // EFFECTS: calculates ticks to beats conversion (reverse of above)
-    public double ticksToBeats(int ticks) {
-        double beats = (double) ticks / (double) sequence.getResolution();
-        ;
-        return beats;
-    }
-
-    // REQUIRES: ticks >= 0
-    // EFFECTS: converts the ticks to which beat the ticks are on
-    public double ticksToOnBeat(int ticks) {
-        return ticksToBeats(ticks) + 1;
+    public void setPlayer(Player p) {
+        player = p;
     }
 
     // EFFECTS: calculates the tick at which the last note ends, this method
@@ -386,44 +184,11 @@ public class Timeline implements Writable {
         return midiTracks;
     }
 
-    // EFFECTS: returns the timeline position in ms by converting ticks to ms
-    public double getPositionMs() {
-        return ticksToMs(positionTick);
-    }
 
-    // EFFECTS: returns the timeline position in beats by converting ticks to beats
-    public double getPositionBeats() {
-        return ticksToBeats(positionTick);
-    }
-
-    // EFFECTS: returns the beat on which the timeline position starts playback
-    public double getPositionOnBeat() {
-        return getPositionBeats() + 1;
-    }
 
     // EFFECTS: returns the track at the specified index from tracks array
     public MidiTrack getTrack(int index) {
         return midiTracks.get(index);
-    }
-
-    public float getBPM() {
-        return bpm;
-    }
-
-    public Sequence getSequence() {
-        return sequence;
-    }
-
-    public Sequencer getSequencer() {
-        return sequencer;
-    }
-
-    public int getPositionTick() {
-        return positionTick;
-    }
-
-    public ArrayList<Integer> getAvaliableChannels() {
-        return avaliableChannels;
     }
 
     public String getProjectName() {
@@ -442,6 +207,19 @@ public class Timeline implements Writable {
         return beatDivision;
     }
 
+    public Player getPlayer() {
+        return player;
+    }
+
+    public double getLengthMs() {
+        return player.getLengthMs();
+    }
+
+    public double getLengthBeats() {
+        return player.getLengthBeats();
+    }
+    
+
     // MODIFIES: this
     // EFFECTS: adds midiTrack to the list of tracks
     public void addMidiTrack(MidiTrack midiTrack) {
@@ -452,7 +230,7 @@ public class Timeline implements Writable {
         EventLog.getInstance().logEvent(e);
     }
 
-    public static PropertyChangeSupport getPropertyChangeSupport() {
+    public PropertyChangeSupport getPropertyChangeSupport() {
         return pcs;
     }
 
@@ -462,9 +240,7 @@ public class Timeline implements Writable {
         JSONObject timelineJson = new JSONObject();
 
         timelineJson.put("projectName", projectName);
-        timelineJson.put("beatsPerMinute", bpm);
-        timelineJson.put("positionTick", positionTick);
-        timelineJson.put("avaliableChannels", new JSONArray(avaliableChannels));
+        timelineJson.put("player", player.toJson());
         timelineJson.put("beatDivision", beatDivision);
         timelineJson.put("beatsPerMeasure", beatsPerMeasure);
         timelineJson.put("horizontalScale", horizontalScale);
