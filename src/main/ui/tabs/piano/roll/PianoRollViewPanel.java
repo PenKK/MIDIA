c62a90805a5e3b9f426ab02f2abcd6eb665f57fc
PenK <spikeb3b@gmail.com>
2025-06-09 15:10:19 -0700

    initial commit

---
+++ src/main/ui/tabs/piano/roll/PianoRollViewPanel.java
@@ -0,0 +1,27 @@
+package ui.tabs.piano.roll;
+
+
+import javax.swing.BoxLayout;
+import javax.swing.JPanel;
+
+import model.TimelineController;
+import ui.ruler.RulerScrollPane;
+import ui.tabs.timeline.midi.TrackPanel;
+
+// JPanel tab that lets the user create notes via graphical interface
+public class PianoRollViewPanel extends JPanel {
+
+    RulerScrollPane rulerScrollPane;
+    PianoRollScrollPane pianoRollScrollPane;
+
+    public PianoRollViewPanel(TimelineController timelineController) {
+        rulerScrollPane = new RulerScrollPane(timelineController);
+        pianoRollScrollPane = new PianoRollScrollPane(timelineController);
+
+        this.setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
+        this.setBorder(TrackPanel.BORDER);
+        this.setAlignmentX(LEFT_ALIGNMENT);
+        this.add(rulerScrollPane);
+        this.add(pianoRollScrollPane);
+    }
+}
