0e1575424614e91a153dae4009ea470de813fbd8
PenK <spikeb3b@gmail.com>
2025-07-23 19:33:53 -0700

    Added a new type of player, BlockPlayer, for the piano roll

---
+++ src/main/model/BlockPlayer.java
@@ -0,0 +1,61 @@
+package model;
+
+import java.awt.event.ActionEvent;
+
+import javax.sound.midi.InvalidMidiDataException;
+import javax.sound.midi.Track;
+
+import model.event.Event;
+import model.event.EventLog;
+
+public class BlockPlayer extends Player {
+
+    private Block block;
+    private MidiTrack parentMidiTrack;
+
+    public BlockPlayer(Block block, MidiTrack parentMidiTrack, float initialBpm) {
+        super();
+        this.block = block;
+        this.parentMidiTrack = parentMidiTrack;
+
+        setBPM(initialBpm);
+    }
+
+    @Override
+    public void updateSequence() throws InvalidMidiDataException {
+        resetTracks();
+        Track track = sequence.createTrack();
+        
+        for (Note note : block.getNotes()) {
+            MidiTrack.applyNoteToTrack(track, note, parentMidiTrack.isPercussive(), 
+                                       parentMidiTrack.getInstrument(), parentMidiTrack.getChannel());
+        }
+
+        sequencer.setSequence(sequence);
+
+        Event e = new Event(String.format("Playback sequence was updated in Block [Piano Roll] with instrument %s", parentMidiTrack.getInstrument()));
+        EventLog.getInstance().logEvent(e);
+    }
+
+    @Override
+    public double getLengthBeats() {
+        return ticksToBeats(block.getDurationTicks());
+    }
+
+    @Override
+    public double getLengthMs() {
+        return ticksToMs(block.getDurationTicks());
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        if (e.getSource().equals(playbackUpdateTimer)) {
+            if (!isDraggingRuler) {
+                updatePositionTick();
+            }
+
+            // timeline.getPropertyChangeSupport().firePropertyChange("positionTick", null, null);
+        }
+    }       
+
+}
